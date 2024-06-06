ee073fed1b4e7449a6e66fe67b616130899fa847
ARIHANT JAIN <91581141+jain-arihant-2002@users.noreply.github.com>
2024-06-06 18:05:52 +0530

    Update apiSlice.js
    Changed Server Location

--- src/app/api/apiSlice.js
+++ src/app/api/apiSlice.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import { setCredentials, logOut } from "../../features/auth/authSlice";
 
 const baseQuery = fetchBaseQuery({
-    baseUrl: "http://localhost:5000",
+    baseUrl: "https://hmsserver-production.up.railway.app/",
     credentials: "include",
     prepareHeaders: (headers, { getState }) => {
         const token = getState().auth.token;
